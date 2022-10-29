0a2c428ceb79a58f896253842d7e1c2641fdd2f0
ADEBZ <86401318+ADEBZ@users.noreply.github.com>
2022-10-29 15:53:33 +0100

    (my week 3 commit)

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    //addition
+    let sum = 5500 + 7310;
+    println!("the sum of 5550 and 7310 is {}",sum);
+
+    //subtraction
+    let difference:f64 = 95.50 - 4.30;
+    println!("the difference of 95.5 and 4.3 = {}",difference ); 
+
+    //multiplication
+    let product:f32 = 4.00 * 30.00;
+    println!("the multiple of 4 and 30 = {}",product );  
+
+    //division
+    let quotient = 56.7/32.2;
+    println!("the division of 56.7 and 32.2 = {}",quotient ); 
+
+    //remainder
+    let remainder = 43 % 5;
+    println!("the remainer of 43 and 5 = {}",remainder );
+}
---
+++ week-3/practice_9/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut fees:i32 = 25_000;//the mut function allows you to change the value of a variable
+    println!("fees is {}",fees );
+
+    fees = 35_000;
+    println!("fees changed is {}",fees);
+}
