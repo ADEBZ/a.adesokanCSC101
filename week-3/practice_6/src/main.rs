be8a8709b6c34de1c4d06dec562f2626bf54c893
ADEBZ <86401318+ADEBZ@users.noreply.github.com>
2023-01-14 14:43:36 +0100

    Delete week-3 directory

--- week-3/practice_6/src/main.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let interest:f32 = 8.0;// Integer assigned to float variable
-    println!("Interest is {}",interest );
-}
--- week-3/practice_9/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let mut fees:i32 = 25_000;//the mut function allows you to change the value of a variable
-    println!("fees is {}",fees );
-
-    fees = 35_000;
-    println!("fees changed is {}",fees);
-}
